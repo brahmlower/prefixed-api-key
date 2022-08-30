35d98882b6c0d6f1c47615e4a111ef2f5c00d78c
Brahm Lower <bplower@gmail.com>
2022-08-29 19:49:10 -0700

    moved controller builder to dedicated module

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ mod prefixed_api_key;
 pub use crate::prefixed_api_key::PrefixedApiKey;
 pub use crate::prefixed_api_key::PrefixedApiKeyError;
 
+mod controller_builder;
+pub use crate::controller_builder::ControllerBuilder;
+
 mod controller;
-pub use crate::controller::ControllerBuilder;
 pub use crate::controller::PrefixedApiKeyController;
