292a126555cdabb5552a8b12ce8fcca74ea24021
Brahm Lower <bplower@gmail.com>
2024-08-20 17:14:53 -0700

    feat: added try_generate_key to safely generate token values

--- src/controller.rs
+++ src/controller.rs
@@ -42,28 +42,51 @@ impl<R: RngCore + Clone, D: Digest + FixedOutputReset> PrefixedApiKeyController<
     }
 
     /// Generates random bytes using the configured random number generator
+    ///
+    /// Can potentially panic depending on the rng source's implementation of [fill_bytes](rand::RngCore::fill_bytes).
     fn get_random_bytes(&self, length: usize) -> Vec<u8> {
         let mut random_bytes = vec![0u8; length];
         let mut rng = self.rng.clone();
-        // TODO: need to use try_fill_bytes to account for problems with the
-        // underlying rng source. typically this will be fine, but the RngCore
-        // docs say errors can arrise, and will cause a panic if you use fill_bytes
         rng.fill_bytes(&mut random_bytes);
         random_bytes
+    }
+
+    /// Tries to generate random bytes using the configured random number generator
+    fn try_get_random_bytes(&self, length: usize) -> Result<Vec<u8>, crate::rand::Error> {
+        let mut random_bytes = vec![0u8; length];
+        let mut rng = self.rng.clone();
+        match rng.try_fill_bytes(&mut random_bytes) {
+            Ok(_) => Ok(random_bytes),
+            Err(err) => Err(err),
+        }
     }
 
     /// Generates a random token for part of the api key. This can be used for generating
     /// both the secret long key, and the shorter plaintext key. The random values are
     /// base58 encoded, which is a key feature/requirement of the library.
+    ///
+    /// Can potentially panic depending on the rng source's implementation of [fill_bytes](rand::RngCore::fill_bytes).
     fn get_random_token(&self, length: usize) -> String {
         let bytes = self.get_random_bytes(length);
         bs58::encode(bytes).into_string()
+    }
+
+    /// Tries to generate a random token for part of the api key. This can be used for
+    /// generating both the secret long key, and the shorter plaintext key. The random values
+    /// are base58 encoded, which is a key feature/requirement of the library.
+    fn try_get_random_token(&self, length: usize) -> Result<String, crate::rand::Error> {
+        match self.try_get_random_bytes(length) {
+            Ok(bytes) => Ok(bs58::encode(bytes).into_string()),
+            Err(err) => Err(err),
+        }
     }
 
     /// Generates a new PrefiexedApiKey using the configured string prefix, short token
     /// prefix (if configured), and random number generator. A hash of the new keys' long
     /// token is not calculated, so you'll still need to create the hash after calling
     /// this function.
+    ///
+    /// Can potentially panic depending on the rng source's implementation of [fill_bytes](rand::RngCore::fill_bytes).
     pub fn generate_key(&self) -> PrefixedApiKey {
         // generate the short token
         let mut short_token = self.get_random_token(self.short_token_length);
@@ -85,12 +108,54 @@ impl<R: RngCore + Clone, D: Digest + FixedOutputReset> PrefixedApiKeyController<
         PrefixedApiKey::new(self.prefix.to_owned(), short_token, long_token)
     }
 
+    /// Tries to generate a new PrefiexedApiKey using the configured string prefix, short
+    /// token prefix (if configured), and random number generator. A hash of the new keys'
+    /// long token is not calculated, so you'll still need to create the hash after calling
+    /// this function.
+    pub fn try_generate_key(&self) -> Result<PrefixedApiKey, crate::rand::Error> {
+        // generate the short token
+        let mut short_token = self.try_get_random_token(self.short_token_length)?;
+
+        // If the short token prefix is configured, concat it and the generated string and
+        // drop any characters beyond the configured short token length
+        if self.short_token_prefix.is_some() {
+            let prefix_string = self.short_token_prefix.as_ref().unwrap().to_owned();
+            short_token = (prefix_string + &short_token)
+                .chars()
+                .take(self.short_token_length)
+                .collect()
+        }
+
+        // Generate the secret long token
+        let long_token = self.try_get_random_token(self.long_token_length)?;
+
+        // Construct and return the new pak
+        let pak = PrefixedApiKey::new(self.prefix.to_owned(), short_token, long_token);
+        Ok(pak)
+    }
+
     /// Generates a new key using the [generate_key](PrefixedApiKeyController::generate_key) function, but also calculates and
     /// returns the hash of the long token.
+    ///
+    /// Can potentially panic depending on the rng source's implementation of [fill_bytes](rand::RngCore::fill_bytes).
     pub fn generate_key_and_hash(&self) -> (PrefixedApiKey, String) {
         let pak = self.generate_key();
         let hash = self.long_token_hashed(&pak);
         (pak, hash)
+    }
+
+    /// Generates a new key using the [try_generate_key](PrefixedApiKeyController::try_generate_key) function, but also calculates and
+    /// returns the hash of the long token.
+    pub fn try_generate_key_and_hash(
+        &self,
+    ) -> Result<(PrefixedApiKey, String), crate::rand::Error> {
+        match self.try_generate_key() {
+            Ok(pak) => {
+                let hash = self.long_token_hashed(&pak);
+                Ok((pak, hash))
+            }
+            Err(err) => Err(err),
+        }
     }
 
     /// Hashes the long token of the provided PrefixedApiKey using the hashing
@@ -104,7 +169,7 @@ impl<R: RngCore + Clone, D: Digest + FixedOutputReset> PrefixedApiKeyController<
 
     /// Secure helper for checking if a given PrefixedApiKey matches a given
     /// long token hash. This uses the hashing algorithm configured on the controller
-    /// and uses the [constant_time_eq](constant_time_eq::constant_time_eq) method of comparing hashes
+    /// and uses the [constant_time_eq](constant_time_eq::constant_time_eq()) method of comparing hashes
     /// to avoid possible timing attacks.
     pub fn check_hash(&self, pak: &PrefixedApiKey, hash: &str) -> bool {
         let pak_hash = self.long_token_hashed(pak);
@@ -135,6 +200,19 @@ mod controller_tests {
         let generator =
             PrefixedApiKeyController::<_, Sha256>::new("mycompany".to_owned(), OsRng, None, 8, 24);
         let token_string = generator.generate_key().to_string();
+        let pak_result = PrefixedApiKey::from_string(&token_string);
+        assert_eq!(pak_result.is_ok(), true);
+        let pak_string = pak_result.unwrap().to_string();
+        assert_eq!(token_string, pak_string);
+    }
+
+    #[test]
+    fn try_generator() {
+        let generator =
+            PrefixedApiKeyController::<_, Sha256>::new("mycompany".to_owned(), OsRng, None, 8, 24);
+        let token_res = generator.try_generate_key();
+        assert!(token_res.is_ok());
+        let token_string = token_res.unwrap().to_string();
         let pak_result = PrefixedApiKey::from_string(&token_string);
         assert_eq!(pak_result.is_ok(), true);
         let pak_string = pak_result.unwrap().to_string();
